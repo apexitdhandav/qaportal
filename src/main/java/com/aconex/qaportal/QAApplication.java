60467891a9d0538a8de785d4a503a0e6fde8d059
Simar Singh <simarsher220@gmail.com>
2018-08-29 16:55:29 +0530

    QAP-001 | Pair:None | Test application run

---
+++ src/main/java/com/aconex/qaportal/QAApplication.java
@@ -0,0 +1,13 @@
+package com.aconex.qaportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QAApplication {
+
+    public static void main(String args[]){
+        SpringApplication.run(QAApplication.class, args);
+    }
+
+}
