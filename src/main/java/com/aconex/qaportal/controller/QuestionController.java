5707e1da723f030b63a248c5208bf51802e68671
Divyanshu Bhati <bhatidivyanshu@hotmail.com>
2018-09-11 10:38:06 +0530

    QAP-102.2 | Pair:None | CORS header file update

--- src/main/java/com/aconex/qaportal/controller/QuestionController.java
+++ src/main/java/com/aconex/qaportal/controller/QuestionController.java
@@ -3,10 +3,14 @@ package com.aconex.qaportal.controller;
 import com.aconex.qaportal.entity.Question;
 import com.aconex.qaportal.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import java.util.List;
 
